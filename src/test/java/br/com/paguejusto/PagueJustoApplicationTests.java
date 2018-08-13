d0535b7d76025112eadecd56db02ddaa412a27cb
Guilherme Freitas <guilhermefreitass@gmail.com>
2018-08-13 07:44:08 -0300

    Projeto criado.

---
+++ src/test/java/br/com/paguejusto/PagueJustoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.paguejusto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PagueJustoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
